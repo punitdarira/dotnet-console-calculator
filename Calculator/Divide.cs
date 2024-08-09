8bed5d3f3e42469fedddf106e026a4fc8039ed42
punitdarira <punitdarira@gmail.com>
2024-08-09 02:45:37 -0400

    Revert "test changes"

--- Calculator/Divide.cs
+++ Calculator/Divide.cs
@@ -1,10 +1,6 @@
 namespace Calculator{
     public class Divide: IBinaryOperator{
         public static float Eval(float numertor, float denominator){
-            float result = 1;
-            result = 2;
-            result = 3;
-            result = 4;
             return numertor/denominator;
         }
     }
